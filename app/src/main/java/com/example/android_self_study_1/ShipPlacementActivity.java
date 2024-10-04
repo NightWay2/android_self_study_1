9d8afb35db6f8c857c0ff9f6e05f16f7ded3a5e3
Maksym Zakomirnyi <138601598+NightWay2@users.noreply.github.com>
2024-10-04 22:28:53 +0300

    refactored project

--- app/src/main/java/com/example/android_self_study_1/ShipPlacementActivity.java
+++ app/src/main/java/com/example/android_self_study_1/ShipPlacementActivity.java
@@ -1,8 +1,6 @@
 package com.example.android_self_study_1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ShipPlacementActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_ship_placement);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.relativeLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button playButton = findViewById(R.id.playButton);
-        playButton.setOnClickListener(v -> openShipReplacementActivity());
-    }
-
-    public void openShipReplacementActivity()
-    {
-        Intent intent = new Intent(MainActivity.this, ShipPlacementActivity.class);
-        startActivity(intent);
     }
 }
